5922a24bbd67af96b1a2ed7d4955af567a970268
ghost <i@nowtiloos.ru>
2024-02-24 23:38:24 +0300

    create users templates adn urls.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def login(request):
+    return render(
+        request=request,
+        template_name='users/login.html'
+    )
+
+
+def registration(request):
+    return render(
+        request=request,
+        template_name='users/registration.html'
+    )
