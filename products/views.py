0410bf240e8f36e74a7b65ebb09c96fad0b4b1a5
ghost <i@nowtiloos.ru>
2024-02-24 20:21:02 +0300

    add data to db

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from products.models import Product, ProductCategory
 
 
 def index(request):
@@ -15,26 +16,8 @@ def index(request):
 def products(request):
     context: dict = {
         'title': 'Магазин одежды каталог',
-        'products': [
-            {
-                'image': '/static/vendor/img/products/Adidas-hoodie.png',
-                'name': 'Худи черного цвета с монограммами adidas Originals',
-                'price': 6090,
-                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни'
-            },
-            {
-                'image': '/static/vendor/img/products/Blue-jacket-The-North-Face.png',
-                'name': 'Синяя куртка The North Face',
-                'price': 23725,
-                'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.'
-            },
-            {
-                'image': '/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',
-                'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
-                'price': 3390,
-                'description': 'Материал с плюшевой текстурой. Удобный и мягкий'
-            }
-        ]
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
     }
     return render(
         request=request,
