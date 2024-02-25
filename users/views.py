013d2a7a33e48896526127788e28ea506d9eb820
ghost <i@nowtiloos.ru>
2024-02-25 11:50:04 +0300

    add forms

--- users/views.py
+++ users/views.py
@@ -1,10 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from users.models import User
+from users.forms import UserLoginForm
+from django.urls import reverse
 
 
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(
+            data=request.POST
+        )
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(
+                username=username,
+                password=password
+            )
+            if user:
+                auth.login(
+                    request=request,
+                    user=user
+                )
+                return HttpResponseRedirect(
+                    redirect_to=reverse(
+                        viewname='index'
+                    )
+                )
+    else:
+        form = UserLoginForm()
+    context: dict = {
+        'form': form
+    }
     return render(
         request=request,
-        template_name='users/login.html'
+        template_name='users/login.html',
+        context=context,
     )
 
 
